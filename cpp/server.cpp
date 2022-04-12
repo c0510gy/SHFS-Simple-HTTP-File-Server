fabc7b6cc6bc3a319f7647d68bf5e34ba9d6580b
Sang-geon Yun <45253879+c0510gy@users.noreply.github.com>
2022-04-12 23:28:01 +0900

    feat: implement templates & simple directing

--- cpp/server.cpp
+++ cpp/server.cpp
@@ -3,18 +3,35 @@
 #include <map>
 #include <fstream>
 #include <sstream>
+#include <filesystem>
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#include "http.h"
+#include "./include/http.h"
+#define FMT_HEADER_ONLY
+#include "./lib/fmt/core.h"
 #define PORT 1234
+
+const std::string ROOT_DIRECTORY = "./root";
 
 using std::cerr;
 using std::map;
 using std::string;
+
+std::string current_directory(std::string path)
+{
+  std::string ret = "";
+
+  int j = path.size();
+  while (j && path[--j] != '/')
+    ;
+  for (int i = 0; i <= j; ++i)
+    ret += path[i];
+  return ret;
+}
 
 std::string readFile(std::string filepath)
 {
@@ -37,11 +54,10 @@ HTTPResponse errorResponse(std::string error_code, std::string error_phrase, std
   response.status_code = error_code;
   response.status_phrase = error_phrase;
 
+  std::string error_template = readFile("./templates/error.html");
+
   response.headers["Connection"] = "close";
-  response.body = "<html><title>Tiny Error</title><body style=\"background-color:"
-                  "ffffff\""
-                  ">" +
-                  error_message + "</body></html>";
+  response.body = fmt::format(error_template, error_code, error_phrase, error_message);
   response.headers["Content-Length"] = std::to_string(response.body.size());
 
   return response;
@@ -72,7 +88,7 @@ std::string parsePath(std::string url)
   }
   if (path == "/")
   {
-    path += "index.html";
+    // path += "index.html";
   }
   return path;
 }
@@ -83,26 +99,67 @@ HTTPResponse requestHandler(char *buffer)
 
   HTTPRequest request = HTTPRequest(req_message);
 
-  string filepath = "./root" + parsePath(request.URL);
-  std::cout << filepath << std::endl;
+  string path = ROOT_DIRECTORY + parsePath(request.URL);
+  std::cout << path << std::endl;
 
-  if (!std::filesystem::exists(filepath))
+  if (!std::filesystem::exists(path))
   {
     return errorResponse("404", "Not found", "Couldn't find this file");
   }
 
-  HTTPResponse response = HTTPResponse();
-  response.protocol = "HTTP/1.0";
-  response.status_code = "200";
-  response.status_phrase = "OK";
+  if (path.back() == '/' && std::filesystem::is_directory(path))
+  {
+    string parent_path = path.size() > ROOT_DIRECTORY.size() + 1
+                             ? current_directory(
+                                   path.substr(0, path.size() - 1))
+                                   .erase(0, ROOT_DIRECTORY.size())
+                             : "";
 
-  response.headers["Connection"] = "close";
-  response.headers["Content-type"] = getType(filepath);
-  response.body = readFile(filepath);
-  // response.body = "<html><head><title>example</title></head><body>Hello, world!</body></html>";
-  response.headers["Content-Length"] = std::to_string(response.body.size());
+    std::cout << "parent_path " << parent_path << std::endl;
 
-  return response;
+    std::string directory_template = readFile("./templates/directory.html");
+
+    std::string list_html = "";
+    if (parent_path.size())
+      list_html += fmt::format("<li><a href=\"{0}\">{1}</a></li>", parent_path, "..");
+    for (auto entry : std::filesystem::directory_iterator(path))
+    {
+      std::string filename = entry.path().string().erase(0, path.size());
+      std::string filepath = filename + (entry.is_directory() ? "/" : "");
+      list_html += fmt::format("<li><a href=\"./{0}\">{1}</a></li>", filepath, filename);
+    }
+    std::string directory_html = fmt::format(directory_template, request.URL, request.URL, list_html);
+
+    HTTPResponse response = HTTPResponse();
+    response.protocol = "HTTP/1.0";
+    response.status_code = "200";
+    response.status_phrase = "OK";
+
+    response.headers["Connection"] = "close";
+    response.headers["Content-type"] = "text/html";
+    response.body = directory_html;
+    response.headers["Content-Length"] = std::to_string(response.body.size());
+
+    return response;
+  }
+
+  if (std::filesystem::is_regular_file(path))
+  {
+    HTTPResponse response = HTTPResponse();
+    response.protocol = "HTTP/1.0";
+    response.status_code = "200";
+    response.status_phrase = "OK";
+
+    response.headers["Connection"] = "close";
+    response.headers["Content-type"] = getType(path);
+    response.body = readFile(path);
+    // response.body = "<html><head><title>example</title></head><body>Hello, world!</body></html>";
+    response.headers["Content-Length"] = std::to_string(response.body.size());
+
+    return response;
+  }
+
+  return errorResponse("404", "Not found", "Couldn't find this file");
 }
 
 int main()
